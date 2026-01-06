44272b680e382660cec239f9d95aa313437f7a4e
Daniel Armando Ciendua Ricartue <daciendua@uniboyaca.edu.co>
2026-01-06 15:00:03 -0500

    Complementos JS e imagenes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
+document.addEventListener("DOMContentLoaded",()=>{
+    const buttonPractice =document.getElementById('information-practice')
+    buttonPractice.onclick = ()=>{
+        const informationAditional = document.getElementById('information-aditional')
+    }
+
+    const buttonExercise = document.getElementById('information-exercise')
+    buttonExercise.onclick = ()=>{
+        alert('Boton presionado en información Ejercicio');
+    }
+    
+    const buttonSound = document.getElementById('information-sound')
+    buttonSound.onclick = ()=>{
+        alert('El Boton de Musica fue presionado')
+    }
+})
 console.log('Estoy listo!');
